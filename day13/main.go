06070e8f80c1a76ee7c02de901f6db9d3b7c5893
Craig Robson <craig@zhatt.com>
2021-12-23 10:18:12 -0800

    --- Day 13: Transparent Origami ---

---
+++ day13/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"zhatt/aoc2021/aoc"
+	"zhatt/aoc2021/coord"
+)
+
+type direction int
+
+const (
+	foldX direction = iota
+	foldY
+)
+
+type instruction struct {
+	direction direction
+	location  int
+}
+
+func newInstruction(direction direction, location int) instruction {
+	return instruction{direction: direction, location: location}
+}
+
+type paper struct {
+	points map[coord.Coord]struct{}
+}
+
+func newPaper() paper {
+	return paper{points: make(map[coord.Coord]struct{})}
+}
+
+func (paper *paper) addPoint(c coord.Coord) {
+	paper.points[c] = struct{}{}
+}
+
+func (paper *paper) fold(instruction instruction) {
+	if instruction.direction == foldX {
+		for point := range paper.points {
+			if point.Xval > instruction.location {
+				delete(paper.points, point)
+				newPoint := coord.Coord{
+					Xval: instruction.location - (point.Xval - instruction.location),
+					Yval: point.Yval,
+				}
+
+				paper.points[newPoint] = struct{}{}
+			}
+		}
+
+	}
+	if instruction.direction == foldY {
+		for point := range paper.points {
+			if point.Yval > instruction.location {
+				delete(paper.points, point)
+				newPoint := coord.Coord{
+					Xval: point.Xval,
+					Yval: instruction.location - (point.Yval - instruction.location),
+				}
+
+				paper.points[newPoint] = struct{}{}
+			}
+		}
+
+	}
+}
+
+func (paper *paper) numberOfPoints() int {
+	return len(paper.points)
+}
+
+func (paper paper) String() string {
+	coords := make([]coord.Coord, 0, len(paper.points))
+
+	for coord := range paper.points {
+		coords = append(coords, coord)
+	}
+
+	minBound := coord.MinBound(coords)
+	maxBound := coord.MaxBound(coords)
+
+	output := ""
+	for y := minBound.Yval; y <= maxBound.Yval; y++ {
+		if y != 0 {
+			output += "\n"
+		}
+		for x := minBound.Xval; x <= maxBound.Xval; x++ {
+			if _, exists := paper.points[coord.Coord{Xval: x, Yval: y}]; exists {
+				output += "#"
+			} else {
+				output += " "
+			}
+		}
+	}
+	return output
+}
+
+func parseInput(inputLines []string) (paper, []instruction) {
+	paper := newPaper()
+	instructions := make([]instruction, 0)
+
+	for index, line := range inputLines {
+		if line == "" {
+			inputLines = inputLines[index+1:]
+			break
+		}
+		tokens := strings.Split(line, ",")
+		xval, err := strconv.Atoi(tokens[0])
+		aoc.PanicOnError(err)
+		yval, err := strconv.Atoi(tokens[1])
+		aoc.PanicOnError(err)
+		paper.addPoint(coord.Coord{Xval: xval, Yval: yval})
+	}
+
+	for _, line := range inputLines {
+		tokens := strings.Split(line, "=")
+		direction := foldX
+		if tokens[0][len(tokens[0])-1] == 'y' {
+			direction = foldY
+		}
+		amount, err := strconv.Atoi(tokens[1])
+		aoc.PanicOnError(err)
+
+		instruction := newInstruction(direction, amount)
+		instructions = append(instructions, instruction)
+	}
+
+	return paper, instructions
+}
+
+func part1(inputList []string) string {
+	paper, instructions := parseInput(inputList)
+	paper.fold(instructions[0])
+	count := paper.numberOfPoints()
+	return strconv.Itoa(count)
+}
+
+func part2(inputList []string) string {
+	paper, instructions := parseInput(inputList)
+	for _, instruction := range instructions {
+		paper.fold(instruction)
+	}
+
+	return fmt.Sprintln(paper)
+}
+
+func main() {
+	aoc.MainFunc(part1, part2)
+}
---
+++ day13/main_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"zhatt/aoc2021/aoc"
+	"zhatt/aoc2021/coord"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var exampleInput1 = []string{
+	"6,10",
+	"0,14",
+	"9,10",
+	"0,3",
+	"10,4",
+	"4,11",
+	"6,0",
+	"6,12",
+	"4,1",
+	"0,13",
+	"10,12",
+	"3,4",
+	"3,0",
+	"8,4",
+	"1,10",
+	"2,14",
+	"8,10",
+	"9,0",
+	"",
+	"fold along y=7",
+	"fold along x=5",
+}
+
+func TestParseInput(t *testing.T) {
+	paper, instructions := parseInput(exampleInput1)
+	assert.Equal(t, 18, len(paper.points))
+	assert.Contains(t, paper.points, coord.Coord{Xval: 6, Yval: 10})
+	assert.Contains(t, paper.points, coord.Coord{Xval: 9, Yval: 0})
+	assert.Contains(t, paper.points, coord.Coord{Xval: 0, Yval: 13})
+
+	assert.Equal(t, []instruction{
+		newInstruction(foldY, 7),
+		newInstruction(foldX, 5)}, instructions)
+}
+
+func TestPart1Example1(t *testing.T) {
+	result := part1(exampleInput1)
+	assert.Equal(t, "17", result)
+}
+
+func TestPart1Input(t *testing.T) {
+	result := part1(aoc.ReadInput("input.txt"))
+	assert.Equal(t, "743", result)
+}
+
+func TestPart2Example1(t *testing.T) {
+	var expected = []string{
+		"#####\n",
+		"#   #\n",
+		"#   #\n",
+		"#   #\n",
+		"#####\n",
+	}
+
+	result := part2(exampleInput1)
+	assert.Equal(t, strings.Join(expected, ""), result)
+}
+
+func TestPart2Input(t *testing.T) {
+	var expected = []string{
+		"###   ##  ###  #     ##  #  # #  # #   \n",
+		"#  # #  # #  # #    #  # # #  #  # #   \n",
+		"#  # #    #  # #    #  # ##   #### #   \n",
+		"###  #    ###  #    #### # #  #  # #   \n",
+		"# #  #  # #    #    #  # # #  #  # #   \n",
+		"#  #  ##  #    #### #  # #  # #  # ####\n",
+	}
+	result := part2(aoc.ReadInput("input.txt"))
+	assert.Equal(t, strings.Join(expected, ""), result)
+}
