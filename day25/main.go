d2ba0bd444fd5c9c0b1f0e5300265997d5c691a0
Craig Robson <craig@zhatt.com>
2022-02-06 21:59:53 -0800

    --- Day 25: Sea Cucumber ---

---
+++ day25/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"strconv"
+	"zhatt/aoc2021/aoc"
+)
+
+type cucumberMap struct {
+	sizeX int
+	sizeY int
+	data  [][]string
+}
+
+func newCucumberMap(sizeX int, sizeY int) *cucumberMap {
+
+	cucMap := &cucumberMap{
+		sizeX: sizeX,
+		sizeY: sizeY,
+		data:  make([][]string, 0, sizeX),
+	}
+
+	for x := 0; x < sizeX; x++ {
+		cucMap.data = append(cucMap.data, make([]string, sizeY))
+	}
+
+	return cucMap
+}
+
+func parseInput(inputLines []string) *cucumberMap {
+	sizeX := len(inputLines[0])
+	sizeY := len(inputLines)
+
+	m := newCucumberMap(sizeX, sizeY)
+
+	for y, line := range inputLines {
+		for x := range line {
+			m.data[x][y] = line[x : x+1]
+		}
+	}
+
+	return m
+}
+
+func (m *cucumberMap) String() string {
+	s := ""
+	for y := 0; y < m.sizeY; y++ {
+		if y != 0 {
+			s += "\n"
+		}
+		for x := 0; x < m.sizeX; x++ {
+			s += m.data[x][y]
+		}
+	}
+
+	return s
+}
+
+func (m *cucumberMap) moveEast() {
+	newMap := newCucumberMap(m.sizeX, m.sizeY)
+
+	for y := 0; y < m.sizeY; y++ {
+		for x := 0; x < m.sizeX; x++ {
+			var toX = x + 1
+			if x == m.sizeX-1 {
+				toX = 0
+			}
+			if m.data[x][y] == ">" && m.data[toX][y] == "." {
+				newMap.data[x][y] = "."
+				newMap.data[toX][y] = ">"
+				x++ // Skip ahead since we've already filled.
+			} else {
+				newMap.data[x][y] = m.data[x][y]
+			}
+		}
+	}
+
+	m.data = newMap.data
+}
+
+func (m *cucumberMap) moveDown() {
+	newMap := newCucumberMap(m.sizeX, m.sizeY)
+
+	for x := 0; x < m.sizeX; x++ {
+		for y := 0; y < m.sizeY; y++ {
+			var toY = y + 1
+			if y == m.sizeY-1 {
+				toY = 0
+			}
+			if m.data[x][y] == "v" && m.data[x][toY] == "." {
+				newMap.data[x][y] = "."
+				newMap.data[x][toY] = "v"
+				y++ // Skip ahead since we've already filled.
+			} else {
+				newMap.data[x][y] = m.data[x][y]
+			}
+		}
+	}
+
+	m.data = newMap.data
+}
+
+func (m *cucumberMap) move() {
+	m.moveEast()
+	m.moveDown()
+}
+
+func part1(inputList []string) string {
+	cucumberMap := parseInput(inputList)
+
+	steps := 0
+
+	for {
+		steps++
+
+		current := cucumberMap.String()
+		cucumberMap.move()
+		next := cucumberMap.String()
+
+		if current == next {
+			break
+		}
+	}
+
+	return strconv.Itoa(steps)
+}
+
+func part2(inputList []string) string {
+	//	burrow := parseInput(inputList, part2e)
+
+	return strconv.Itoa(0)
+}
+
+func main() {
+	aoc.MainFunc(part1, part2)
+}
---
+++ day25/main_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"strings"
+	"testing"
+	"zhatt/aoc2021/aoc"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var exampleInput1 = []string{
+	"...>>>>>...",
+}
+
+var exampleInput2 = []string{
+	"..........",
+	".>v....v..",
+	".......>..",
+	"..........",
+}
+
+var exampleInput3 = []string{
+	"...>...",
+	".......",
+	"......>",
+	"v.....>",
+	"......>",
+	".......",
+	"..vvv..",
+}
+
+var exampleInput4 = []string{
+	"v...>>.vv>",
+	".vv>>.vv..",
+	">>.>v>...v",
+	">>v>>.>.v.",
+	"v>v.vv.v..",
+	">.>>..v...",
+	".vv..>.>v.",
+	"v.v..>>v.v",
+	"....v..v.>",
+}
+
+func TestParseInput(t *testing.T) {
+	cucumberMap1 := parseInput(exampleInput1)
+	assert.Equal(t, strings.Join(exampleInput1, "\n"), cucumberMap1.String())
+
+	cucumberMap2 := parseInput(exampleInput2)
+	assert.Equal(t, strings.Join(exampleInput2, "\n"), cucumberMap2.String())
+}
+
+func TestPart1Example1(t *testing.T) {
+	cucumberMap := parseInput(exampleInput1)
+
+	cucumberMap.move()
+	assert.Equal(t, "...>>>>.>..", cucumberMap.String())
+
+	cucumberMap.move()
+	assert.Equal(t, "...>>>.>.>.", cucumberMap.String())
+}
+
+func TestPart2Example2(t *testing.T) {
+	cucumberMap := parseInput(exampleInput2)
+
+	expectedStep1 := "..........\n.>........\n..v....v>.\n.........."
+	cucumberMap.move()
+	assert.Equal(t, expectedStep1, cucumberMap.String())
+}
+
+func TestPart2Example3(t *testing.T) {
+	cucumberMap := parseInput(exampleInput3)
+
+	expectedStep4 := ">......\n..v....\n..>.v..\n.>.v...\n...>...\n.......\nv......"
+
+	cucumberMap.move()
+	cucumberMap.move()
+	cucumberMap.move()
+	cucumberMap.move()
+
+	assert.Equal(t, expectedStep4, cucumberMap.String())
+}
+
+func TestPart2Example4(t *testing.T) {
+	result := part1(exampleInput4)
+
+	assert.Equal(t, "58", result)
+}
+
+func TestPart1Input(t *testing.T) {
+	result := part1(aoc.ReadInput("input.txt"))
+	assert.Equal(t, "482", result)
+}
+
+/*
+func TestPart2Example1(t *testing.T) {
+	result := part2(exampleInput)
+	assert.Equal(t, "44169", result)
+}
+
+func TestPart2Input(t *testing.T) {
+	result := part2(aoc.ReadInput("input.txt"))
+	assert.Equal(t, "43481", result)
+}
+*/
